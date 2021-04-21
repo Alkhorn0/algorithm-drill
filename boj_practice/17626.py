2e4d011fe76ce1850f97a8e2b12f12034a637059
Alkhorn0 <lobby0517@gmail.com>
2021-04-21 22:19:34 +0900

    dp practice

---
+++ boj_practice/17626.py
@@ -0,0 +1,14 @@
+# 동적계획법 (다이나믹 프로그래밍)
+n = int(input())
+dp = [0, 1]
+
+for i in range(2, n+1):
+    min_value = 1e9
+    j = 1
+
+    while (j**2) <= i:
+        min_value = min(min_value, dp[i-(j**2)])
+        j += 1
+
+    dp.append(min_value + 1)
+print(dp[n])
