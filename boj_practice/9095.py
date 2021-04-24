f9bf46e53f41a126ea7466210a53e3e9ff984e27
Alkhorn0 <lobby0517@gmail.com>
2021-04-24 16:41:36 +0900

    dp문제

---
+++ boj_practice/9095.py
@@ -0,0 +1,11 @@
+# dp문제
+T = int(input())
+dp = [0]*11
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+for t in range(T):
+    n = int(input())
+    for i in range(4, n+1):
+        dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+    print(dp[n])
