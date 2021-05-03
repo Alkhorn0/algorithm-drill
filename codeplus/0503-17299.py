60f15959a12e3ef10b7095d2743dcb682cc3c8a9
Alkhorn0 <lobby0517@gmail.com>
2021-05-03 21:55:11 +0900

    설명 추가

--- codeplus/0503-17299.py
+++ codeplus/0503-17299.py
@@ -7,11 +7,11 @@ ans = [0]*n
 for i in a:
     f[i] += 1
 for j in range(n):
-    if not s:
+    if not s:       # 스택이 비어있는 경우 일단 저장(a[j]의 오등큰수 x)
         s.append(j)
     while s and f[a[s[-1]]] < f[a[j]]:
-        ans[s.pop()] = a[j]
-    s.append(j)
-while s:
+        ans[s.pop()] = a[j] # 스택의 top과 비교 하면서 오등큰수에 해당하면 넣어줌
+    s.append(j)     # a[j]의 오등큰수를 못찾았으므로 추가해줌
+while s:    # 한번 돌고 아직 스택에 남아있으면 ngf(i)=-1
     ans[s.pop()] = -1
 print(' '.join(map(str, ans)))
