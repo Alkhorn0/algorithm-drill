15b7cd3ed28921df44c308c5306d341f7af5267c
Alkhorn0 <lobby0517@gmail.com>
2021-05-09 16:02:48 +0900

    0509 복습 필기

--- codeplus/0507-13398.py
+++ codeplus/0507-13398.py
@@ -16,8 +16,9 @@ for i in range(n-1, -1, -1):
         continue
     if dr[i] < dr[i+1] + a[i]:
         dr[i] = dr[i+1] + a[i]
-ans = max(d)
+ans = max(d)    # 숫자 삭제 안하는 경우
 for i in range(1, n-1):
+    # 삭제를 해서 합이 더 커지면 바꿈
     if ans < d[i-1] + dr[i+1]:
         ans = d[i-1] + dr[i+1]
 print(ans)
--- codeplus/0507-17404.py
+++ codeplus/0507-17404.py
@@ -6,15 +6,19 @@ d = [[0]*3 for _ in range(n+1)]
 ans = 1000*1000 + 1
 for k in range(3):
     for j in range(3):
+        # 마을의 형태가 원형이므로 1번과 n번이 같은 색으로 칠해지면 안되므로 미리 1번의 색을 정해두고 시작
         if j == k:
             d[1][j] = a[1][j]
         else:
+            # 정한색 이외의 색이 선택되는 경우의 수를 없애기 위한 큰 수 대입
             d[1][j] = 1000*1000+1
+    # 직선 마을 풀듯이 품
     for i in range(2, n+1):
         d[i][0] = min(d[i-1][1], d[i-1][2]) + a[i][0]
         d[i][1] = min(d[i-1][0], d[i-1][2]) + a[i][1]
         d[i][2] = min(d[i-1][0], d[i-1][1]) + a[i][2]
     for j in range(3):
+        # n번집과 1번집의 색의 같은 경우는 제외
         if j == k:
             continue
         ans = min(ans, d[n][j])
