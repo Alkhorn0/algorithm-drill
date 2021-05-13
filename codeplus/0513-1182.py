12f61cc69ba2a5c35fc94798f41b0607aaccad0e
Alkhorn0 <lobby0517@gmail.com>
2021-05-13 09:52:29 +0900

    0513

---
+++ codeplus/0513-1182.py
@@ -0,0 +1,11 @@
+n, s = map(int, input().split())
+a = list(map(int, input().split()))
+ans = 0
+for i in range(1, 1 << n):
+    sum = 0
+    for k in range(n):
+        if i & (1 << k):
+            sum += a[k]
+    if sum == s:
+        ans += 1
+print(ans)
