5b27ad481c0264926bbaa5b2cc78be4b7ec58287
Alkhorn0 <lobby0517@gmail.com>
2021-06-24 09:33:13 +0900

    수정중

--- codeplus/0623-16909.py
+++ codeplus/0623-16909.py
@@ -1,4 +1,4 @@
-# 다시보기
+# 다시보기, 틀림
 import sys
 input = sys.stdin.readline
 
@@ -13,41 +13,41 @@ rg = [n+1]*(n+1)
 ls = [0]*(n+1)
 rs = [n+1]*(n+1)
 sg, ss = [], []
-sg.append((1, a[1]))
-ss.append((1, a[1]))
+sg.append([1, a[1]])
+ss.append([1, a[1]])
 for i in range(2, n+1):
     while sg != [] and a[i] > sg[-1][1]:
         rg[sg[-1][0]] = i
         sg.pop()
-    sg.append((i, a[i]))
+    sg.append([i, a[i]])
     while ss != [] and a[i] <= ss[-1][1]:
         rs[ss[-1][0]] = i
         ss.pop()
-    ss.append((i, a[i]))
+    ss.append([i, a[i]])
 
 sg, ss = [], []
-sg.append((n, a[n]))
-ss.append((n, a[n]))
-for i in range(n-1, 0):
+sg.append([n, a[n]])
+ss.append([n, a[n]])
+for i in range(n, 0, -1):
     while sg != [] and a[i] > sg[-1][1]:
         lg[sg[-1][0]] = i
         sg.pop()
-    sg.append((i, a[i]))
+    sg.append([i, a[i]])
     while ss != [] and a[i] < ss[-1][1]:
         ls[ss[-1][0]] = i
         ss.pop()
-    ss.append((i, a[i]))
+    ss.append([i, a[i]])
 
 for i in range(1, n+1):
     l = min(i, lg[i]+1)
     r = max(i, rg[i]-1)
     length = r-l+1
     ans += (calc(length) - calc(r-i) - calc(i-l))*a[i]
-    print(ans)
+    
 for i in range(1, n+1):
     l = min(i, ls[i]+1)
     r = max(i, rs[i]-1)
     length = r-l+1
     ans -= (calc(length) - calc(r-i) - calc(i-l))*a[i]
-    print(ans)
+    
 print(ans)
