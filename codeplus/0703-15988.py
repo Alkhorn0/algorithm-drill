6631f18eb0625e30b3c7504fb955aa6b3ce15dd1
Alkhorn0 <lobby0517@gmail.com>
2021-07-03 21:01:36 +0900

    Update 0703-15988.py

--- codeplus/0703-15988.py
+++ codeplus/0703-15988.py
@@ -1,14 +1,11 @@
-import sys
-sys.setrecursionlimit(1000000)
-
-dp = [0]*1000001
-dp[1] = 1
-dp[2] = 2
-dp[3] = 4
-for i in range(4, 1000001):
-    dp[i] = (dp[i-1]+dp[i-2]+dp[i-3])
-
-t = int(input())
-for _ in range(t):
+d = [0]*(1000001)
+d[1] = 1
+d[2] = 2
+d[3] = 4
+mod = 1000000009
+for i in range(4, 1000000+1):
+    d[i] = (d[i-3]+d[i-2]+d[i-1])%mod
+T = int(input())
+for _ in range(T):
     n = int(input())
-    print(dp(n))
+    print(d[n])
